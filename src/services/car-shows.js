25b92daa4828a89b41d10cfcb3a28f4013b85364
Ken Sodemann <ken@ionic.io>
2018-09-01 16:16:43 -0500

    feat(car-shows): add an enpoint to get a single car show

--- src/services/car-shows.js
+++ src/services/car-shows.js
@@ -9,16 +9,28 @@ module.exports = class CarShows {
       client.query('select * from car_shows order by year desc'),
       client.query('select * from car_show_classes')
     ]);
-    const cars = qres[0].rows.map(car => ({
-      ...car,
-      classes: qres[1].rows
-        .filter(cls => cls.car_show_rid === car.id)
-        .map(cls => {
-          delete cls.car_show_rid;
-          return cls;
-        })
-    }));
     client.release();
-    return cars;
+    return merge(qres[0].rows, qres[1].rows);
+  }
+
+  async get(id) {
+    const client = await this._pool.connect();
+    const qres = await Promise.all([
+      client.query('select * from car_shows where id = $1', [id]),
+      client.query('select * from car_show_classes where car_show_rid = $1', [id])
+    ]);
+    client.release();
+    return merge(qres[0].rows, qres[1].rows)[0];
   }
 };
+
+function merge(cars, classes) {
+  return cars.map(car => ({
+    ...car,
+    classes: classes.filter(cls => cls.car_show_rid === car.id)
+      .map(cls => {
+        delete cls.car_show_rid;
+        return cls;
+      })
+  }));
+}
--- test/services/car-shows.spec.js
+++ test/services/car-shows.spec.js
@@ -13,7 +13,7 @@ describe('service: car-classes', () => {
   beforeEach(() => {
     pool = new MockPool();
     sinon.stub(pool.test_client, 'query');
-    pool.test_client.query.returns({rows: []});
+    pool.test_client.query.returns({ rows: [] });
     testData = {
       carShows: [
         {
@@ -290,6 +290,96 @@ describe('service: car-classes', () => {
     it('releases the client', async () => {
       sinon.spy(pool.test_client, 'release');
       await service.getAll();
+      expect(pool.test_client.release.calledOnce).to.be.true;
+    });
+  });
+
+  describe('get', () => {
+    it('connects to the pool', () => {
+      sinon.spy(pool, 'connect');
+      service.get(3);
+      expect(pool.connect.calledOnce).to.be.true;
+    });
+
+    it('queries the car shows and classes', async () => {
+      await service.get(3);
+      expect(pool.test_client.query.calledTwice).to.be.true;
+      expect(
+        pool.test_client.query.calledWith(
+          'select * from car_shows where id = $1',
+          [3]
+        )
+      ).to.be.true;
+      expect(
+        pool.test_client.query.calledWith(
+          'select * from car_show_classes where car_show_rid = $1',
+          [3]
+        )
+      ).to.be.true;
+    });
+
+    it('returns undefined if there is no car show', async () => {
+      const data = await service.get(42);
+      expect(data).to.be.undefined;
+    });
+
+    it('returns the data for the car show', async () => {
+      pool.test_client.query
+        .onCall(0)
+        .returns(Promise.resolve({ rows: [testData.carShows[2]] }));
+      pool.test_client.query.onCall(1).returns(
+        Promise.resolve({
+          rows: testData.carShowClasses.filter(cls => cls.car_show_rid === 3)
+        })
+      );
+      const data = await service.get(3);
+      expect(data).to.deep.equal({
+        id: 3,
+        name: 'Waukesha Show 2017',
+        date: '2017-08-10',
+        year: 2017,
+        classes: [
+          {
+            id: 9,
+            name: 'A',
+            description: 'Antique through 1954, Cars & Trucks'
+          },
+          {
+            id: 10,
+            name: 'B',
+            description: '1955-1962, Cars Only'
+          },
+          {
+            id: 11,
+            name: 'C',
+            description: '1963-1967, Cars Only'
+          },
+          {
+            id: 12,
+            name: 'D',
+            description: '1968-1970, Cars Only'
+          }
+        ]
+      });
+    });
+
+    it('returns the data for a car show without classes', async () => {
+      pool.test_client.query
+        .onCall(0)
+        .returns(Promise.resolve({ rows: [testData.carShows[2]] }));
+      const data = await service.get(3);
+      expect(data).to.deep.equal({
+        id: 3,
+        name: 'Waukesha Show 2017',
+        date: '2017-08-10',
+        year: 2017,
+        classes: []
+      });
+    });
+
+    it('releases the client', async () => {
+      sinon.spy(pool.test_client, 'release');
+      await service.get(3);
       expect(pool.test_client.release.calledOnce).to.be.true;
     });
   });
