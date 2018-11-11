03c0e1df81702974ca5fdd48d1d8a9f6ce2731f4
Ken Sodemann <ken@ionic.io>
2018-11-11 14:42:14 -0600

    refactor(tests): use testing database

--- test/services/car-classes.spec.js
+++ test/services/car-classes.spec.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const MockClient = require('../util/mock-client');
 const database = require('../../src/config/database');
-const sinon = require('sinon');
 const service = require('../../src/services/car-classes');
 const testDatabase = require('../util/test-database');
 const testData = require('../util/test-data');
@@ -13,38 +11,15 @@ describe('service: car-classes', () => {
     await testDatabase.reload();
   });
 
+  afterEach(() => {
+    expect(database.idleCount).to.equal(database.totalCount);
+  });
+
   describe('getAll', () => {
-    it('connects to the database', () => {
-      sinon.spy(database, 'connect');
-      service.getAll();
-      expect(database.connect.calledOnce).to.be.true;
-      database.connect.restore();
-    });
-
-    it('queries the car classes', async () => {
-      const client = new MockClient();
-      sinon.stub(database, 'connect');
-      database.connect.resolves(client);
-      sinon.spy(client, 'query');
-      await service.getAll();
-      expect(client.query.calledOnce).to.be.true;
-      database.connect.restore();
-    });
-
     it('returns the data', async () => {
       const data = await service.getAll();
       expect(data.length).to.equal(testData.classes.length);
       expect(data).to.deep.equal(testData.classes);
-    });
-
-    it('releases the client', async () => {
-      const client = new MockClient();
-      sinon.stub(database, 'connect');
-      database.connect.resolves(client);
-      sinon.spy(client, 'release');
-      await service.getAll();
-      expect(client.release.calledOnce).to.be.true;
-      database.connect.restore();
     });
   });
 });
